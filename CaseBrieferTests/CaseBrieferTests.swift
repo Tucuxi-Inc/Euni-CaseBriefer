4fd02e672f33d676e75c84f908864d790f374bdb
Kevin-Tucuxi <kevin@tucuxi.ai>
2025-03-04 08:07:19 -0800

    Initial Commit

---
+++ CaseBrieferTests/CaseBrieferTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaseBrieferTests.swift
+//  CaseBrieferTests
+//
+//  Created by Kevin Keller on 3/4/25.
+//
+
+import Testing
+@testable import CaseBriefer
+
+struct CaseBrieferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
